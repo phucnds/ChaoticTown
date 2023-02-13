89652e916ee17b69be3c2b5d9b40e6c975dbb757
phucnds <nduongphuc@gmail.com>
2023-02-13 13:55:35 +0700

    - update

---
+++ Assets/_Data/Scripts/Actions/ActionBasic.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBasic : ScriptableObject
+{
+    public string id;  //Id for the save file
+
+    private static List<ActionBasic> list = new List<ActionBasic>();
+
+    public virtual void StartAction()
+    {
+        //Inherit this function to run code when the character start the action
+    }
+
+    public virtual void StopAction()
+    {
+        //Inherit this function to run code when the character stop the action
+    }
+
+    public virtual void UpdateAction()
+    {
+        //Inherit this function to run code every frame while doing this action
+    }
+
+    public virtual void TriggerAction()
+    {
+        //Custom action trigger
+    }
+
+    public virtual bool CanDoAction()
+    {
+        return true; 
+    }
+
+    public static void Load(string folder = "")
+    {
+        list.Clear();
+        list.AddRange(Resources.LoadAll<ActionBasic>(folder));
+    }
+
+    public static ActionBasic Get(string id)
+    {
+        foreach (ActionBasic action in list)
+        {
+            if (action.id == id)
+                return action;
+        }
+        return null;
+    }
+
+    public static T Get<T>() where T : ActionBasic
+    {
+        foreach (ActionBasic action in list)
+        {
+            if (action is T)
+                return (T)action;
+        }
+        return null;
+    }
+
+    public static List<ActionBasic> GetAll()
+    {
+        return list;
+    }
+}
---
+++ Assets/_Data/Scripts/Actions/ActionTakeAWalk.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Action", menuName = "ChaoticTown/Actions/TakeAWalk", order = 10)]
+public class ActionTakeAWalk : ActionBasic
+{
+    public override void StartAction()
+    {
+        base.StartAction();
+    }
+
+    public override void UpdateAction()
+    {
+        base.UpdateAction();
+    }
+
+    public override void StopAction()
+    {
+        base.StopAction();
+    }
+
+    public override bool CanDoAction()
+    {
+        return base.CanDoAction();
+    }
+}
