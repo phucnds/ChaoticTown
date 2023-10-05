591662ebc3a629179f85108e6de1295b500a3cee
phucnds <42528099+phucnds@users.noreply.github.com>
2023-10-05 15:01:55 +0700

    - update

--- Assets/_Data/Scripts/SceneManament/ScriptableObjects/InitializationSO.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Initialization", menuName = "Scene Data/Initialization")]
-public class InitializationSO : GameSceneSO { }
